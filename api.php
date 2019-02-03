2db89287b964c2e94f4286415c3a937c108d1789
tempsteve <tempsteve1024@gmail.com>
2019-02-03 18:01:29 +0800

    Optimize.

--- api.php
+++ api.php
@@ -1,191 +1,146 @@
 <?php
-define("SITE", "Your API Site");
-define("API_KEY", "Your API KEY");
+class Mailchimp
+{
+    public $apiKey;
 
-function listCreate() {
-    $data = array(
-        "name" => "Your List",
-        "contact" => array(
-           "company" => "YourCompany",
-           "address1" => "No. 87, Test Rd.",
-           "city" => "Taipei",
-           "state" => "TPE",
-           "zip" => "100",
-           "country" => "TW"
-        ),
-        "permission_reminder" => "This is a test.",
-        "campaign_defaults" => array(
-           "from_name" => "Your Name",
-           "from_email" => "YourEmail@mail.com",
-           "subject" => "",
-           "language" => "en"
-        ),
-        "email_type_option" => true
-    );
-    $post_json = json_encode($data);
-    $ch = curl_init(SITE."lists");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        array(
-            'Content-Type: application/json',
-            'Authorization: apikey '.API_KEY
-        )
-    );
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $result_decode = json_decode($result);
-    if (isset($result_decode->{"id"})) {
-        return $result_decode->{"id"};
-    } else {
-        return false;
+    public function __construct($apiKey) {
+        $this->apiKey = $apiKey;
+        $server = explode("-", $this->apiKey);
+        $this->site = "https://".$server[1].".api.mailchimp.com/3.0/";
     }
 
-}
-
-function listMemberCreate($email, $list_id) {
-    $data = array(
-        'email_address' => $email,
-        'status' => 'subscribed',
-        'tags' => array('a tag')
-    );
-    $post_json = json_encode($data);
-    $ch = curl_init(SITE."lists/".$list_id."/members");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        array(
-            'Content-Type: application/json',
-            'Authorization: apikey '.API_KEY
-        )
-    );
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $result_decode = json_decode($result);
-    if (isset($result_decode->{"email_address"})) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function campaignCreate($list_id) {
-    $ch = curl_init(SITE."lists/".$list_id."/segments");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        array(
-            'Authorization: apikey '.API_KEY
-        )
-    );
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $result_decode = json_decode($result);
-    $segment_id = $result_decode->{"segments"}[0]->{"id"};
-
-    $data = array(
-        'type' => 'regular',
-        'recipients' => array(
-            'list_id' => $list_id,
-            'segment_opts' => array(
-                'saved_segment_id' => $segment_id
-            )
-        ),
-        'settings' => array(
-            'subject_line' => 'The Mailchimp Test Campaign',
-            'reply_to' => 'YourEmail@mail.com',
-            'from_name' => 'Your Name'
-        )
-    );
-    $post_json = json_encode($data);
-    $ch = curl_init(SITE."campaigns");
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        array(
-            'Content-Type: application/json',
-            'Authorization: apikey '.API_KEY
-        )
-    );
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $result_decode = json_decode($result);
-    if (isset($result_decode->{"id"})) {
-        return $result_decode->{"id"};
-    } else {
-        return false;
-    }
-}
-
-function campaignContentUpdate($campaign_id) {
-    $content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-         nisi ut aliquip ex ea commodo consequat.
-         Duis aute irure dolor in reprehenderit in voluptate velit esse
-         cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-         cupidatat non proident, sunt in culpa qui officia deserunt
-         mollit anim id est laborum.";
-    $data = array('html' => $content);
-    $post_json = json_encode($data);
-    $ch = curl_init(SITE."campaigns/".$campaign_id."/content");
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_json);
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        array(
-            'Content-Type: application/json',
-            'Authorization: apikey '.API_KEY
-        )
-    );
-    curl_exec($ch);
-    curl_close($ch);
-}
-
-function campaignSend($campaign_id) {
-    $ch = curl_init(SITE."campaigns/".$campaign_id."/send-checklist");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        array(
-            'Authorization: apikey '.API_KEY
-        )
-    );
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $result_decode = json_decode($result);
-
-    if ($result_decode->{"is_ready"} === true) {
-        $ch = curl_init(SITE."campaigns/".$campaign_id."/actions/send");
-        curl_setopt($ch, CURLOPT_POST, 1);
+    function curl($method, $route, $postData) {
+        $ch = curl_init($this->site.$route);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt(
-            $ch,
-            CURLOPT_HTTPHEADER,
-            array(
-                'Authorization: apikey '.API_KEY
+        if ($method == "POST") {
+            curl_setopt($ch, CURLOPT_POST, 1);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+            curl_setopt(
+                $ch,
+                CURLOPT_HTTPHEADER,
+                array(
+                    'Content-Type: application/json',
+                    'Authorization: apikey '.$this->apiKey
+                )
+            );
+        } elseif ($method == "PUT") {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+            curl_setopt(
+                $ch,
+                CURLOPT_HTTPHEADER,
+                array(
+                    'Content-Type: application/json',
+                    'Authorization: apikey '.$this->apiKey
+                )
+            );
+        } elseif ($method == "GET") {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Authorization: apikey '.$this->apiKey));
+        }
+        $result = curl_exec($ch);
+        curl_close($ch);
+        return $result;
+    }
+
+    function listCreate() {
+        $data = array(
+            "name" => "Your List",
+            "contact" => array(
+               "company" => "YourCompany",
+               "address1" => "No. 87, Test Rd.",
+               "city" => "Taipei",
+               "state" => "TPE",
+               "zip" => "100",
+               "country" => "TW"
+            ),
+            "permission_reminder" => "This is a test.",
+            "campaign_defaults" => array(
+               "from_name" => "Your Name",
+               "from_email" => "YourEmail@mail.com",
+               "subject" => "",
+               "language" => "en"
+            ),
+            "email_type_option" => true
+        );
+        $post_json = json_encode($data);
+        $result = $this->curl("POST", "lists", $post_json);
+        $result_decode = json_decode($result);
+        if (isset($result_decode->{"id"})) {
+            return $result_decode->{"id"};
+        } else {
+            return false;
+        }
+    }
+
+    function listMemberCreate($email, $list_id) {
+        $data = array(
+            'email_address' => $email,
+            'status' => 'subscribed',
+            'tags' => array('a tag')
+        );
+        $post_json = json_encode($data);
+        $result = $this->curl("POST", "lists/".$list_id."/members", $post_json);
+        $result_decode = json_decode($result);
+        if (isset($result_decode->{"email_address"})) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function campaignCreate($list_id) {
+        $result = $this->curl("GET", "lists/".$list_id."/segments", "");
+        $result_decode = json_decode($result);
+        $segment_id = $result_decode->{"segments"}[0]->{"id"};
+        $data = array(
+            'type' => 'regular',
+            'recipients' => array(
+                'list_id' => $list_id,
+                'segment_opts' => array(
+                    'saved_segment_id' => $segment_id
+                )
+            ),
+            'settings' => array(
+                'subject_line' => 'The Mailchimp Test Campaign',
+                'reply_to' => 'YourEmail@mail.com',
+                'from_name' => 'Your Name'
             )
         );
-        curl_exec($ch);
-        curl_close($ch);
+        $post_json = json_encode($data);
+        $result = $this->curl("POST", "campaigns", $post_json);
+        $result_decode = json_decode($result);
+        if (isset($result_decode->{"id"})) {
+            return $result_decode->{"id"};
+        } else {
+            return false;
+        }
+    }
 
-        return true;
-    } else {
-        echo $result."<br>";
-        return false;
+    function campaignContentUpdate($campaign_id) {
+        $content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+             nisi ut aliquip ex ea commodo consequat.
+             Duis aute irure dolor in reprehenderit in voluptate velit esse
+             cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+             cupidatat non proident, sunt in culpa qui officia deserunt
+             mollit anim id est laborum.";
+        $data = array('html' => $content);
+        $post_json = json_encode($data);
+        $result = $this->curl("PUT", "campaigns/".$campaign_id."/content", $post_json);
+    }
+
+    function campaignSend($campaign_id) {
+        $result = $this->curl("GET", "campaigns/".$campaign_id."/send-checklist", "");
+        $result_decode = json_decode($result);
+
+        if ($result_decode->{"is_ready"} === true) {
+            $result = $this->curl("POST", "campaigns/".$campaign_id."/actions/send", "");
+            return true;
+        } else {
+            echo $result."<br>";
+            return false;
+        }
     }
 }
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
 include_once "api.php";
+$Mailchimp = new Mailchimp("Your API key here");
+
 // Step 1: create a new list
-$list_id = listCreate();
+$list_id = $Mailchimp->listCreate();
 echo "List ".$list_id." created!<br>";
 
 // Step 2: add my email to the list
 $my_mail = "tempsteve@mail-apps.com";
-if (listMemberCreate($my_mail, $list_id) === true)
+if ($Mailchimp->listMemberCreate($my_mail, $list_id) === true)
     echo $my_mail." added!<br>";
 
 // Step 3: add another email addresses to the list
@@ -15,18 +17,18 @@ for ($i=0; $i < 10; $i++) {
     array_push($email_list, md5(mt_rand())."@abc.com");
 }
 foreach ($email_list as $email) {
-    if (listMemberCreate($email, $list_id) === true)
+    if ($Mailchimp->listMemberCreate($email, $list_id) === true)
         echo $email." added!<br>";
 }
 
 // Step 4: create a new campaign
-$campaign_id = campaignCreate($list_id);
+$campaign_id = $Mailchimp->campaignCreate($list_id);
 echo "Campaign ".$campaign_id." created!<br>";
 
 // Step 5: edit campaign's content
-campaignContentUpdate($campaign_id);
+$Mailchimp->campaignContentUpdate($campaign_id);
 
 // Step 6: send a campaign email to all the members in the list
-if (campaignSend($campaign_id) === true)
+if ($Mailchimp->campaignSend($campaign_id) === true)
     echo "Sent!<br>";
 ?>
